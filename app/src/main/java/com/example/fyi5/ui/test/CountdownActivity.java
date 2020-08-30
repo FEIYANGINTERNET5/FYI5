7ce47a71842a104e66d5962653f8f0e63590f362
Jasminennn <2391331180@qq.com>
2020-08-30 20:19:58 +0800

    zn:test commit

---
+++ app/src/main/java/com/example/fyi5/ui/test/CountdownActivity.java
@@ -0,0 +1,16 @@
+package com.example.fyi5.ui.test;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.fyi5.R;
+
+public class CountdownActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_countdown);
+    }
+}
